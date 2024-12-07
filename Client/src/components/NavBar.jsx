10afa0222da4f55b4784c5e91fde7ce53b98e512
Ahmet Yasir Özcan <ahmetyasirozcan@Ahmet-MacBook-Air.local>
2024-12-07 19:53:24 +0300

    logo added

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import IndexPage from '../routes/Index';
+import logo from '../assets/logos/notifica.png';
 
 
 function NavBar() {
   return (
-    <nav className='flex justify-between bg-[#1e2203] text-white p-4 items-center w-full'>
-      <div className=''>NOTİFİCA</div>
+    <nav className='flex justify-between bg-[#1e2203] text-white py-4 px-10 items-center w-full'>
+      <div className=''>
+        <img src={logo} alt="Logo" className='w-30 h-10'/>
+      </div>
       <div>
       </div>
       <div>
